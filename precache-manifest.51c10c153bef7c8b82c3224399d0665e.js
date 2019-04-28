7df01c5a1ea704b3ef5156c6f7ec76f3a05c2666
Tim <tkolenda@gmail.com>
2019-04-28 11:55:38 -0400

    Updates

---
+++ precache-manifest.51c10c153bef7c8b82c3224399d0665e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2115f0ec7467f7c135769e94d654e6",
+    "url": "/goal-based-task-list/index.html"
+  },
+  {
+    "revision": "38a6052303f8d2ecafc9",
+    "url": "/goal-based-task-list/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/goal-based-task-list/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "38a6052303f8d2ecafc9",
+    "url": "/goal-based-task-list/static/js/main.c742f4c2.chunk.js"
+  },
+  {
+    "revision": "2dba89cf95f82afe97f1",
+    "url": "/goal-based-task-list/static/js/runtime~main.1b4fb08f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goal-based-task-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
